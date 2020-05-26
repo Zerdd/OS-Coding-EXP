b1a8f8e282e2cfe7975f0198ce80e495eaf62252
zeitlin <zerd669@gmail.com>
2020-05-26 11:15:38 +0800

    final editon

--- exp1/1_1.cpp
+++ exp1/1_1.cpp
@@ -6,81 +6,114 @@
 
 using namespace std;
 
-class CA_LL
+class Content
 {
 public:
-
-  int i = 1;
-
-  bool Serialize(const char *pFilePath)
+  Content()
   {
-    // 打开文件，不存在就创建
-    // if (fd = open(pFilePath, O_WRONLY | O_CREAT | O_TRUNC) == -1)   
-    int fd = open(pFilePath, O_WRONLY | O_CREAT | O_TRUNC);
-    if (fd == -1 ) {
-      cout << "Open error!" << endl;
-      return false;
-    }
-
-    cout << "open fd:" << fd << endl;
-
-    // 写入
-    if (write(fd, &i, sizeof(int)) == -1) {
-      close(fd);
-      cout << "Write error!" << endl;
-      return false;
-    }
-
-    cout << "writed size:" << fd << endl;
-
-    // 关闭
-    if (close(fd) == -1) {
-      cout << "Close error!" << endl;
-      return false;
-    }
-
-    cout << "Serialize success!" << endl;
-    return true;
+    i = 0;
   }
 
-
-  bool Deserialize(const char *pFilePath)
+  void SetContent(int j)
   {
-    int fd = open(pFilePath, O_RDONLY);
-    if (fd == -1) {
-      close(fd);
-      cout << "Open error!" << endl;
-      return false;
-    }
-
-    if (read(fd, &i, sizeof(int)) == -1) {
-      close(fd);
-      cout << "Read error!" << endl;
-      return false;
-    }
-
-    if (close(fd) == -1) {
-      cout << "Close error!" << endl;
-      return false;
-    }
-
-    cout << "Deserialize success!" << endl;
-    cout << "a.i = " << i << endl;
-    return true;
+    i = j;
   }
+
+  virtual ~Content() {}
+
+  void ShowContent()
+  {
+    std::cout << "Show_Content: " << i << std::endl;
+  }
+
+  bool Serialize(const char *pFilePath);
+  bool Deserialize(const char *pFilePath);
+
+private:
+  int i;
 };
+
+bool Content::Serialize(const char *pFilePath)
+{
+  // 打开文件，不存在就创建
+  int fd = open(pFilePath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  if (fd == -1)
+  {
+    cout << "Open error!" << endl;
+    return false;
+  }
+
+  cout << "open fd:" << fd << endl;
+
+  // 写入
+  if (write(fd, &i, sizeof(int)) == -1)
+  {
+    close(fd);
+    cout << "Write error!" << endl;
+    return false;
+  }
+
+  cout << "writed size:" << fd << endl;
+
+  // 关闭
+  if (close(fd) == -1)
+  {
+    cout << "Close error!" << endl;
+    return false;
+  }
+
+  cout << "Serialize success!" << endl;
+  return true;
+}
+
+bool Content::Deserialize(const char *pFilePath)
+{
+  // 打开已序列化的文件
+  int fd = open(pFilePath, O_RDONLY);
+  if (fd == -1)
+  {
+    close(fd);
+    cout << "Open error!" << endl;
+    return false;
+  }
+
+  if (read(fd, &i, sizeof(int)) == -1)
+  {
+    close(fd);
+    cout << "Read error!" << endl;
+    return false;
+  }
+
+  if (close(fd) == -1)
+  {
+    cout << "Close error!" << endl;
+    return false;
+  }
+
+  cout << "Deserialize success!" << endl;
+  cout << "a.i = " << i << endl;
+  return true;
+}
 
 int main()
 {
-  CA_LL a;
-  a.i = 20;
+  Content a;
+  a.SetContent(10);
 
-  if (a.Serialize("data") == false) {
-    cout << "Serialize error!" << endl;  
+  {
+    if (a.Serialize("data") == false)
+    {
+      cout << "Serialize error!" << endl;
+    }
   }
 
-  if (a.Deserialize("data") == false) {
-    cout << "Deserialize error!" << endl;
+  cout << "----------------" << endl;
+
+  {
+    if (a.Deserialize("data") == false)
+    {
+      cout << "Deserialize error!" << endl;
+    }
   }
 
   return 0;
